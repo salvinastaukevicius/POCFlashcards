d600040c73c336973e8cb693b614b47ce7c71b8a
Salvinas <i481136@fhict.nl>
2024-06-21 14:24:47 +0200

    Initial Commit

---
+++ POCFlashcards/POCFlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCFlashcardsApp.swift
+//  POCFlashcards
+//
+//  Created by Salvinas Taukevicius on 21/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct POCFlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
